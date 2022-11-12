0fc910784ceb8c806a5c923fe3818ac3c6c47a67
lgolikova <milagoul@gmail.com>
2022-11-12 10:46:47 +0300

    added images

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import hero from "./HeroesData";
 
 function Card(props) {
     return (
         <>
-            <div><img src="{props.url}" /></div>
+            <div><img src={props.url} alt="hero-img" /></div>
             <h2>{props.name}</h2>
             <div>Род деятельности: {props.activity}</div>
             <div>Друзья: {props.friends}</div>
             <div>Суперсила: {props.force}</div>
+            <hr />
         </>
     );
 }
