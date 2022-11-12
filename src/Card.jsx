cd756bd8fef9d5c655fb8c112eca7eb1f22d46d1
lgolikova <milagoul@gmail.com>
2022-11-12 13:15:42 +0300

    br

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,11 +3,15 @@ import React from "react";
 function Card(props) {
     return (
         <>
+            <br />
+            <br />
             <div><img src={props.url} alt="hero-img" /></div>
             <h2>{props.name}</h2>
             <div>Род деятельности: {props.activity}</div>
             <div>Друзья: {props.friends}</div>
             <div>Суперсила: {props.force}</div>
+            <br />
+            <br />
             <hr />
         </>
     );
